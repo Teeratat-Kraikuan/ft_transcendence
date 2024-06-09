a9dce59e74a13881268bc20fa9fa37569bb4666b
cxsx-9 <99903983+cxsx-9@users.noreply.github.com>
2024-06-09 23:08:35 +0700

    fix call profile picture from media file

--- django/project/urls.py
+++ django/project/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	path('', include('home.urls')),
@@ -23,4 +25,4 @@ urlpatterns = [
 	path('users/', include("users.urls")),
 	path('game/', include("game.urls")),
 	path('chat/', include("chatapp.urls")),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
