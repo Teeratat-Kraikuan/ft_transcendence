016c6d0623679c5d4d53d070b51afdb1ecdca042
Teeratat-Kraikuan <lovekoonq@gmail.com>
2024-06-15 17:34:31 +0700

    fix bug cannot access users/friend/ page

--- django/users/urls.py
+++ django/users/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
 	path('callback/', views.callback, name='callback'),
 	path('send_friend_request/<int:userID>/', views.send_friend_request, name='send friend request'),
 	path('accept_friend_request/<int:requestID>/', views.accept_friend_request, name='accept friend request'),
+	path('friend/', views.friend, name='friend'),
 	path('<str:username>/', views.profile, name='profile'),
-	path('friend', views.friend, name='friend'),
 ]
