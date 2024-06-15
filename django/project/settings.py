49d8346788a0fe0485095672ba0ced568ed2785b
Teeratat-Kraikuan <lovekoonq@gmail.com>
2024-06-15 16:50:55 +0700

    change back to development

--- django/project/settings.py
+++ django/project/settings.py
@@ -75,7 +75,6 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -166,8 +165,6 @@ STATICFILES_DIRS = [
 	BASE_DIR / "static"
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/' # Public URL at the browser
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory where uploaded media is saved.
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,9 +10,9 @@ then
 	python /home/django/ft_transcendence/manage.py makemigrations
 	python /home/django/ft_transcendence/manage.py migrate
 	python manage.py collectstatic
-	# python /home/django/ft_transcendence/manage.py runserver 0.0.0.0:8000
+	python /home/django/ft_transcendence/manage.py runserver 0.0.0.0:8001
 	# python /home/django/ft_transcendence/manage.py runserver_plus --cert-file /home/django/ft_transcendence/https/cert.pem --key-file /home/django/ft_transcendence/https/key.pem 0.0.0.0:8000
-	daphne -b 0.0.0.0 -p 8001 project.asgi:application
+	# daphne -b 0.0.0.0 -p 8001 project.asgi:application
 	# daphne -b 0.0.0.0 -p 8001 -e ssl:8443:privateKey=/home/django/ft_transcendence/https/key.pem:certKey=/home/django/ft_transcendence/https/cert.pem project.asgi:application --proxy-headers
 fi
 
