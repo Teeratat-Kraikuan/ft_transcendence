ed7fcce8f193ddd90c2f239df26e2a7920ee6e6e
Teeratat-Kraikuan <lovekoonq@gmail.com>
2024-06-17 14:40:26 +0700

    edit images

--- django/game/views.py
+++ django/game/views.py
@@ -11,43 +11,34 @@ def game(request):
 def pong(request):
 	context = {}
 	username = request.user.username if request.user.is_authenticated else 'guest'
-	context['default_image'] = 'default_profile_image.png'
+	context['default_image'] = '/media/profile_pics/default_profile_image.png'
 	context['username'] = username
-	context['profile_image'] = request.user.profile_image if request.user.is_authenticated else 'default_profile_image.png'
+	context['profile_image'] = request.user.profile_image if request.user.is_authenticated else '/media/profile_pics/default_profile_image.png'
 	if request.method == "POST":
-		if request.POST.get('mode') == 'singleplayer':
-			# print('singleplayer', request.POST.get('difficulty'))
-			if request.POST.get('difficulty') == 'easy':
-				pass
-			elif request.POST.get('difficulty') == 'medium':
-				pass
-			elif request.POST.get('difficulty') == 'hard':
-				pass
-		elif request.POST.get('mode') == 'multiplayer':
-			# print('multiplayer', request.POST.get('type'))
-			if request.POST.get('type') == 'create':
-				created = False
-				while not created:
-					room_code = str(random.randint(111111,999999))
-					pong_room, created = PongGame.objects.get_or_create(room_code=room_code, player1=username)
-				context['playerNo'] = 1
-				context['room_code'] = room_code
-				return render(request, "pong.html", context)
-			elif request.POST.get('type') == 'join':
-				room_code = request.POST.get('room_code')
-				try:
-					pong_room = PongGame.objects.get(room_code=room_code)
-					if pong_room.player2 != "to-be-decide":
-						messages.error(request, "that room is full")
-						return redirect('game')
-					pong_room.player2 = username
-					pong_room.save()
-					context['playerNo'] = 2
-					context['room_code'] = room_code
-					return render(request, "pong.html", context)
-				except:
-					messages.error(request, "no room with this code")
+		if request.POST.get('type') == 'create':
+			created = False
+			while not created:
+				room_code = str(random.randint(111111,999999))
+				pong_room, created = PongGame.objects.get_or_create(room_code=room_code, player1=username)
+			context['playerNo'] = 1
+			context['room_code'] = room_code
+			return render(request, "pong.html", context)
+		elif request.POST.get('type') == 'join':
+			room_code = request.POST.get('room_code')
+			try:
+				pong_room = PongGame.objects.get(room_code=room_code)
+				if pong_room.player2 != "to-be-decide":
+					messages.error(request, "that room is full")
 					return redirect('game')
+				pong_room.player2 = username
+				pong_room.save()
+				context['playerNo'] = 2
+				context['room_code'] = room_code
+				print('game start')
+				return render(request, "pong.html", context)
+			except:
+				messages.error(request, "no room with this code")
+				return redirect('game')
 	return render(request, "pong.html", context)
 
 def pong_ai(request):
