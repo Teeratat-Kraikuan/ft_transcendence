388679da6fc36f8c753344a9437840f0d534c854
Tanawat Jukmongkol <tanawat.jukmon@gmail.com>
2024-12-01 17:29:40 +0700

    fixed blunder 😱

--- django/user/urls.py
+++ django/user/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	path('signup/', views.signup, name='signup'),
 	path('logout/', views.logout, name='login'),
 	path('users/<int:user_id>/', views.user, name='user'),
+	path('oauth/', views.oauth, name='oauth'),
 ]
--- django/user/views.py
+++ django/user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
+from django.middleware.csrf import get_token
+from requests_oauthlib import OAuth2Session
+import os
 
 # Create your views here.
 
@@ -31,3 +34,37 @@ def user(req, user_id):
 	return render(req, 'user.html', {
 		"user_id": user_id
 	})
+
+def oauth(req):
+	client_id = os.environ["CLIENT_ID"]
+	client_secret = os.environ["CLIENT_SECRET"]
+	redirect_uri = "https://localhost:8443/oauth"
+	code = req.GET.get("code")
+	state = req.GET.get("state")
+	csrf_token = get_token(req)
+	oauth = OAuth2Session(
+		client_id,
+		redirect_uri=redirect_uri
+	)
+	if not code:
+		authorization_url, state = oauth.authorization_url(
+			'https://api.intra.42.fr/oauth/authorize',
+			kwargs = {
+				'csrfToken': csrf_token
+			}
+		)
+		return redirect(authorization_url)
+	elif state:
+		token = oauth.fetch_token(
+			'https://api.intra.42.fr/oauth/token',
+			client_secret=client_secret,
+			code=code,
+			kwargs = {
+				'csrfToken': csrf_token
+			}
+		)
+		user_data = oauth.get('https://api.intra.42.fr/v2/me').json()
+		login = user_data.get('login')
+		email = user_data.get('email')
+		return redirect("/")
+	return redirect("/login")
